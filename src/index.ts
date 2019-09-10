21985d6a4679c5402de2c00d095ec38682d4f02f
Zé Lucas Chociai <jose@anthor.com.br>
2019-09-10 18:43:37 -0300

    Removed unused code.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import { JunoSDK } from "./sdk";
-
 export * from "./enums";
 export * from "./structs";
 export * from "./inputs";
 export * from "./responses";
 export * from "./resources";
 export * from "./sdk";
-
-const juno = new JunoSDK();
-
-(async () => {
-  await juno.transfer.requestTransfer("734012371EA9DB41EE3574677CE8FEB8CD3E38BAF3BCFA9AACFD78051915A27B", 1);
-})();
