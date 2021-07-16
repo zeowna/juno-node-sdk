f1297d5c0a478497d1edaa768b1ae2952d8a208d
Vytor Calixto <vytor@anthor.com.br>
2021-07-16 11:43:15 -0300

    feat: create BankAccountType enum

--- api-v2/src/entities/BankAccount.ts
+++ api-v2/src/entities/BankAccount.ts
@@ -1,10 +1,12 @@
+import { BankAccountType } from '../enums';
 import { AccountHolder } from './AccountHolder';
 import { JunoEntity } from './JunoEntity';
 
 export interface BankAccount extends JunoEntity {
+  ispb?: string;
   accountComplementNumber: string;
   accountNumber: string;
-  accountType: string;
+  accountType: BankAccountType;
   agencyNumber: string;
   bankNumber: string;
   accountHolder?: AccountHolder;
